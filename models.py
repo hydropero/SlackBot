b3fa6c8f097af3ee7c9df5ad885690edd7a9b920
Myles Bulla <dev.mbulla@gmail.com>
2023-03-11 21:32:15 -0600

    added create statement to models

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Time, Boolean, Column, Integer, String, Float, Date, ForeignKey
 from sqlalchemy.orm import relationship
-from db import Base
+from db import Base, engine
 
 class Test_Table(Base):
     __tablename__ = "testtable"
     id = Column(Integer, primary_key=True, index=True)
     username = Column('username', String, unique=True, index=False)
     password = Column('password', String, unique=False, index=False)
-    
+
+Base.metadata.create_all(engine)
