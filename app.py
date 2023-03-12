18af8342d0b8ecde2cb8922fce73f221505d0c85
Myles Bulla <dev.mbulla@gmail.com>
2023-03-11 23:41:20 -0600

    missed one

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ def handle_submission(ack, body, client, view, logger):
     # then sending the user a verification of their submission
     current_date = datetime.datetime.now
     current_time = datetime.datetime.time
-    do_yesterday = view["state"]["value"]["do_yesterday"]["plain_text_input-action"]
+    do_yesterday = view["state"]["values"]["do_yesterday"]["plain_text_input-action"]
     tech_do_yesterday = view["state"]["values"]["tech_do_yesterday"]["plain_text_input-action"]
     tech_type = view["state"]["values"]["tech_type"]["plain_text_input-action"]
     challenge = view["state"]["values"]["challenge"]["plain_text_input-action"]
